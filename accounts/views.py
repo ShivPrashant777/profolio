60448edc1a90d648e9e9e5ed82d66212227a35f1
ShivPrashant777 <shivprashant777@gmail.com>
2020-01-10 23:10:06 +0530

    Signup Working
    Need to Add data to different Table

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 
 def index(request):
     return render(request, "index.html")
@@ -10,4 +10,15 @@ def login(request):
 
 
 def signup(request):
-    return render(request, "signup.html")
+    if request.method == 'POST':
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        user = User.objects.create_user(username=username, password=password1)
+        user.save()
+        print('User Created')
+        return redirect('/')
+        
+    else:
+        return render(request, "signup.html")
