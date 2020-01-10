4d3d392902a1cefde9719ce97df59251256b5800
Nikhil-2019 <55291138+Nikhil-2019@users.noreply.github.com>
2020-01-10 22:34:58 +0530

    modelsRegistered

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class UserAccounts(models.Model):
+class UserAccount(models.Model):
     name = models.CharField(max_length=50)
     password = models.CharField(max_length=10)
