d1490377afa428118db472490d27cb559924296b
ShivPrashant777 <shivprashant777@gmail.com>
2020-01-14 21:23:48 +0530

    File Uploads Added

--- user/views.py
+++ user/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from django.core.files.storage import FileSystemStorage
 
 
 def logout(request):
     auth.logout(request)
     return redirect('/')
 
+
 def userpage(request):
+    if request.method == 'POST':
+        uploaded_file = request.FILES["document"]
+        fs = FileSystemStorage()
+        fs.save(uploaded_file.name, uploaded_file)
     return render(request, 'user.html')
+
+
